4421b672cd5134cf958eb7307a80f0eee09298ba
Christopher Jones <christopherjones72521@gmail.com>
2015-04-06 13:01:38 -0400

    Submission of files to String
    make tests is having issues for some reason

---
+++ test_length.cpp
@@ -0,0 +1,41 @@
+#include "string.hpp"
+#include <cassert>
+
+//===========================================================================
+int main ()
+{
+	{
+        //------------------------------------------------------
+        // SETUP FIXTURE
+
+        // TEST
+		String str;
+
+        // VERIFY
+        assert(str.length() == 0);
+	}
+
+	{
+		// Setup
+		String str('a');
+
+		// Verify
+		assert(str.length() == 1);
+	}
+
+	{
+		// Setup
+		String str("a");
+
+		// Verify
+		assert(str.length() == 1);
+	}
+
+	{
+		// Test
+		String str("It's over 9000!");
+
+        // Verify
+        assert(str.length() == 15);
+	}
+}
